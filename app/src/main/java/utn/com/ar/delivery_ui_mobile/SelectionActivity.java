737ebaf41b45d1ebbc5e7090f9ef6869c1609ba2
Juanca <clavijojc@gmail.com>
2016-11-07 00:12:21 -0300

    se agrega permanencia de pedido

--- app/src/main/java/utn/com/ar/delivery_ui_mobile/SelectionActivity.java
+++ app/src/main/java/utn/com/ar/delivery_ui_mobile/SelectionActivity.java
@@ -1,6 +1,7 @@
 package utn.com.ar.delivery_ui_mobile;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -17,15 +18,26 @@ public class SelectionActivity extends AppCompatActivity {
         EditText etBebida = (EditText) this.findViewById(R.id.etBebida);
         EditText etDessert = (EditText) this.findViewById(R.id.etPostre);
 
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
+        SharedPreferences.Editor editor = pref.edit();
+
+
+
         Intent myIntent = getIntent();
         if (myIntent != null){
             if(myIntent.getStringExtra("comida") != null)
-                etFood.setText(myIntent.getStringExtra("comida").toString());
+                editor.putString("comida", myIntent.getStringExtra("comida").toString());
             if(myIntent.getStringExtra("bebida") != null)
-                etBebida.setText(myIntent.getStringExtra("bebida").toString());
+                editor.putString("bebida", myIntent.getStringExtra("bebida").toString());
             if(myIntent.getStringExtra("postre") != null)
-                etDessert.setText(myIntent.getStringExtra("postre").toString());
+                editor.putString("postre", myIntent.getStringExtra("postre").toString());
         }
+
+        editor.commit();
+
+        etFood.setText(pref.getString("comida", ""));
+        etBebida.setText(pref.getString("bebida", ""));
+        etDessert.setText(pref.getString("postre", ""));
     }
 
 
